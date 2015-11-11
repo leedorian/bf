801ae3a16acbcfb1186b5d831a08d6e08a29c452
leedorian <leedorian@gmail.com>
2015-11-11 18:31:58 +0800

    backbone

---
+++ src/script/models/todo.js
@@ -0,0 +1,21 @@
+var Backbone = require('backbone');
+var $ = require('jquery');
+Backbone.$ = $;
+
+module.exports = Backbone.Model.extend({
+    initialize: function () {
+
+    },
+    defaults: {
+        title: '',
+        completed: false
+    },
+    parse: function(response, options){
+        return response;
+    },
+    toggle: function(){
+        this.save({
+            completed: !this.get('completed')
+        });
+    }
+});
