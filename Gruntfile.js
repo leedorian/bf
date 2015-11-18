fc97eb93fa200b80bb38789694f99a4410b7d00b
leedorian <leedorian@gmail.com>
2015-11-18 10:07:31 +0800

    merged from master

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,18 +1,30 @@
 'use strict';
-module.exports = function(grunt) {
+module.exports = function (grunt) {
 
-  // Look for grunt config files in the 'grunt' directory
-  require('load-grunt-config')(grunt);
+    // Look for grunt config files in the 'grunt' directory
+    require('load-grunt-config')(grunt);
 
-  grunt.registerTask('default', [
-    'clean:build',
-    'browserify:vendor',
-    'concurrent'
+    grunt.registerTask('default', [
+        'buildAll',
+        'concurrent'
   ]);
-   
- grunt.registerTask('colony',[
-     'shell:colony',
-     'browserSync:colony'
+    grunt.registerTask('buildAll',[
+        'clean:build',
+        'jshint',
+        'jscs',
+        'browserify:vendor',
+        'browserify:app',
+        'browserify:common',
+        'exorcise:dev',
+        'clean:built',
+        'compass:dist',
+        'htmlbuild'
+    ]);
+    /**
+     * Generate the dependencies graph with colony
+     */
+    grunt.registerTask('colony', [
+        'shell:colony',
+        'browserSync:colony'
  ]);
 };
-
